eacd297d0c5546b75389f78cd41d296c1886c659
arslaanKhurshid <arslaankhurshiedsmt231@gmail.com>
2024-11-10 11:25:54 +0530

    socket error

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 
+import cors from 'cors';
+
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from './routes/user.routes.js';
@@ -15,6 +17,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json()); // Middleware for parsing JSON
 app.use(cookieParser());
+
+app.use(cors());
 
 app.use('/api/auth', authRoutes); // Registering auth routes
 app.use("/api/messages", messageRoutes);
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,20 @@
+import Conversations from "./Conversations";
+import LogoutButton from "./LogoutButton";
+import SearchInput from "./SearchInput";
+
+const Sidebar = () => {
+	return (
+		<div className='border-r border-slate-500 p-4 flex flex-col'>
+			<SearchInput />
+			<div className='divider px-3'></div>
+			<Conversations />
+			<LogoutButton />
+		</div>
+	);
+};
+export default Sidebar;
+
+// STARTER CODE FOR THIS FILE
 // import Conversations from "./Conversations";
 // import LogoutButton from "./LogoutButton";
 // import SearchInput from "./SearchInput";
@@ -13,22 +30,3 @@
 // 	);
 // };
 // export default Sidebar;
-
-// STARTER CODE FOR THIS FILE
-import Conversations from "./Conversations";
-import LogoutButton from "./LogoutButton";
-import SearchInput from "./SearchInput";
-
-const Sidebar = () => {
-	return (
-		<div className='border-r border-slate-500 p-4 flex flex-col'>
-			<SearchInput />
-			<div className='divider px-3'></div>
-            <Conversations />
-            <LogoutButton/>
-            
-			
-		</div>
-	);
-};
-export default Sidebar;
